6c28a6b8317c8658552a4dfac12dfd180d0a1e76
dupeyloops <dupeyloops@gmail.com>
2022-08-02 15:32:08 -0400

    2007 Mode Foundation
    Moving this by-hand recreation of a layout I designed in 2007 to the side.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("titlebox").style.fontSize = "10px";
+	document.getElementById("titlebox").style.height = "10px";
+  	document.getElementById("titlebox").style.textAlign = "center";
+  } else {
+    document.getElementById("titlebox").style.fontSize = "30px";
+	document.getElementById("titlebox").style.height = "145px";
+	document.getElementById("titlebox").style.textAlign = "left";
+  }
+}
+
+/*
+function scrollFunctionBACKUP() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("titlebox").style.fontSize = "10px";
+	document.getElementById("titlebox").style.height = "10px";
+  } else {
+    document.getElementById("titlebox").style.fontSize = "30px";
+	document.getElementById("titlebox").style.height = "145px";
+  }
+}
+*/
+
 function showContact() {
   document.getElementById("main-contents").innerHTML = "dupeyloops<br><i>email@domain.com</i>";
 }
+
+/*
+  © 2022 Andrew Loy
+  dupeyloops.github.io
+*/
